5ba4300efae89d39e2c1d219b9062b7e4d40a957
nenad0707 <risticnenad.vr@gmail.com>
2024-05-12 15:15:14 +0200

    Added Etag to localStorage

--- TaskTackler/Handlers/CashingHandler.cs
+++ TaskTackler/Handlers/CashingHandler.cs
@@ -14,30 +14,41 @@ namespace TaskTackler.Handlers
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var cacheControl = new CacheControlHeaderValue()
-            {
-                NoCache = true
-
-            };
-            request.Headers.CacheControl = cacheControl;
-
             string uriKey = request.RequestUri!.ToString();
+            Console.WriteLine("[CashingHandler] Sending request.");
 
-            var etag = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", $"etag-{uriKey}");
-
-            if (!string.IsNullOrEmpty(etag))
+            // Provera tipa metode i invalidacija keša za modifikacione operacije
+            if (request.Method != HttpMethod.Get)
             {
-                request.Headers.IfNoneMatch.Add(new EntityTagHeaderValue($"\"{etag}\"", true));
+                // Brisati ETag iz localStorage
+                await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", $"etag-{uriKey}");
+                Console.WriteLine("[CashingHandler] Cache cleared for modification request.");
+            }
+
+            // Provera da li postoji ETag u localStorage za GET zahteve
+            if (request.Method == HttpMethod.Get)
+            {
+                var etag = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", $"etag-{uriKey}");
+                if (!string.IsNullOrEmpty(etag))
+                {
+                    etag = etag.Trim('\"'); // Uklanjanje navodnika ako već postoje
+                    request.Headers.IfNoneMatch.Add(new EntityTagHeaderValue($"\"{etag}\"", false));
+                    Console.WriteLine($"[CashingHandler] ETag from localStorage: \"{etag}\"");
+                }
             }
 
             var response = await base.SendAsync(request, cancellationToken);
+            Console.WriteLine("[CashingHandler] Response received.");
 
-            if (response.Headers.ETag != null)
+            // Sačuvati novi ETag u localStorage ako je dostupan
+            if (response.Headers.ETag != null && request.Method == HttpMethod.Get)
             {
                 await _jsRuntime.InvokeVoidAsync("localStorage.setItem", $"etag-{uriKey}", response.Headers.ETag.Tag);
+                Console.WriteLine($"[CashingHandler] ETag saved: {response.Headers.ETag.Tag}");
             }
 
             return response;
         }
+
     }
 }
